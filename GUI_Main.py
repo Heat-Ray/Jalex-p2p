c899f34f1019bd2dbe3335609d2a5ce93b58a754
Vaibhav Dwivedi <deshbandhu1008@gmail.com>
2019-03-14 22:28:07 +0530

    Initial send and recieve GUI

--- GUI_Main.py
+++ GUI_Main.py
@@ -1,4 +1,35 @@
 from tkinter import *
+from tkinter import filedialog
+import os
+import soc_server
+import soc_client
+
+def chooseFile():
+    filePath = filedialog.askopenfilename() #!filetypes=(("All files", "*.*")) Why this is not working?
+    entry1.insert(0, filePath)
+    print(filePath)
+
+def chooseRadio():
+    if(var.get() == "Send"):
+        entry1.config(state=NORMAL)
+        btn1.config(state=NORMAL)
+    else:
+        entry1.config(state=DISABLED)
+        btn1.config(state=DISABLED)
+
+def startShare():
+    if (var.get() == "Send"):
+        soc_server.startServer(entry1.get())
+    else:
+        soc_client.startClient()
+    
+def checkFileValidity():
+    print("Checking file validity..")
+    if os.path.isfile(entry1.get()):
+        entry1.config(fg="green")
+    else:
+        entry1.config(fg="red")
+    window.after(2000, checkFileValidity)
 
 window = Tk()
 window.title("Jalex-p2p")
@@ -11,9 +42,9 @@ frame1.pack(fill="x", padx=10, pady=10)
 
 frame2 = Frame(window)
 var = StringVar()
-radio1 = Radiobutton(frame2, text="Send a file", variable=var, value="Send", font=("arial", 12), anchor="w", justify="left")
+radio1 = Radiobutton(frame2, text="Send a file", variable=var, value="Send", font=("arial", 12), anchor="w", justify="left", command=chooseRadio)
 radio1.pack(fill="x")
-radio2 = Radiobutton(frame2, text="Recieve a file", variable=var, value="Recieve", font=("Arial",12), anchor="w", justify="left")
+radio2 = Radiobutton(frame2, text="Recieve a file", variable=var, value="Recieve", font=("Arial",12), anchor="w", justify="left", command=chooseRadio)
 radio2.pack(fill="x")
 var.set("Send")
 frame2.pack(fill="x",pady=10)
@@ -24,8 +55,18 @@ lbl2.pack(fill="both", padx=10, pady=10, side=LEFT)
 frame3.pack(fill="both")
 
 frame4 = Frame(window)
-entry1 = Entry(frame4, bd=1, font=("Arial",12))
+entry1 = Entry(frame4, bd=1, font=("Arial",12), width=35)
 entry1.pack(side=LEFT, anchor="n", padx=10, pady=10)
+filePath = ""
+btn1 = Button(frame4, text="Browse", font=("Arial",12), command=chooseFile)
+btn1.pack(pady=10, side=LEFT)
+print(filePath) #TODO:Fix cann't access variable after using global filePath in function chooseFile
 frame4.pack(fill="both")
 
+frame5 = Frame(window)
+btn2 = Button(window, text="Start sharing", font=("Arial",12), command=startShare)
+btn2.pack()
+frame5.pack(fill="both", pady=10)
+
+window.after(2000, checkFileValidity)
 window.mainloop()
--- soc_server.py
+++ soc_server.py
@@ -1,19 +1,27 @@
 import socket               
 
-s = socket.socket()
-host = '' # Get local machine name
-port = 12340                # Reserve a port for your service.
-s.bind((host, port))        # Bind to the port
-fl=open('shiva.jpg','rb')   # Open file in binary mode to send
-d=fl.read(1024)             # Saves 1024 file bytes in list D
+def startServer(filePath):
+   print("Server Started")
+   filename = filePath.split('/')
+   index = len(filename)
+   filenameInBytes = filename[index-1].encode('utf-8')
+   print(filename[index-1]+" "+str(index))
 
-s.listen(5)                 # Now wait for client connection.
-while True:
-   c, addr = s.accept()     # Establish connection with client.
-   print ('Got connection from', addr)
-   while d!=b'':            # Executes till file list D is empty
-      c.send(d)
-      d=fl.read(10240)
-   c.send(b'')
-   c.close()                # Close the connection
+   s = socket.socket()
+   host = '' # Get local machine name
+   port = 12340                # Reserve a port for your service.
+   s.bind((host, port))        # Bind to the port
+   fl=open(filePath,'rb')   # Open file in binary mode to send
+   d=fl.read(1024)             # Saves 1024 file bytes in list D
+
+   s.listen(5)                 # Now wait for client connection.
+   while True:
+      c, addr = s.accept()     # Establish connection with client.
+      print ('Got connection from', addr)
+      c.send(filenameInBytes)
+      while d!=b'':            # Executes till file list D is empty
+         c.send(d)
+         d=fl.read(10240)
+      c.send(b'')
+      c.close()                # Close the connection
 
